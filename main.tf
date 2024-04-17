3cd6e602913bccfd2257e15b10ec07b2beb26076
venkatasuneel <128783453+suneelchenna@users.noreply.github.com>
2024-04-17 14:23:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-module "base_label" {
-  source    = "cloudposse/label/null"
-  version   = "0.25.0"
-  namespace = "ll"
+module "vpc_services" {
+  source = "./modules/vpc"
+  
+  vpc_cidr_block       = var.vpc_cidr_block
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
 }
