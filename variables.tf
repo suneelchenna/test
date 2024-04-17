da58149c5d152bbbc9889f38dabf9a8302830883
venkatasuneel <128783453+suneelchenna@users.noreply.github.com>
2024-04-17 14:24:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,31 +1,13 @@
-variable "aws_region" {
-  description = "AWS region to use"
-  type        = string
-  default     = "us-west-1"
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID for the target AWS account"
-  type        = string
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
 }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Key for the target AWS account"
-  type        = string
-}
-
-variable "aws_session_token" {
-  description = "AWS Session Token for the target AWS account. Required only if authenticating using temporary credentials"
-  type        = string
-  default     = ""
-}
-
-variable "vpc_cidr" {
-  description = "The IPv4 CIDR block to use for the VPC"
-  type        = string
-  default     = "192.170.0.0/20"
-  validation {
-    condition     = tonumber(split("/", var.vpc_cidr)[1]) <= 20 && tonumber(split("/", var.vpc_cidr)[1]) >= 16
-    error_message = "CIDR size must be at least /20 and no larger than /16"
-  }
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
 }
