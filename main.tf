805a469b5731748ded148f565282ac83959e3759
venkatasuneel <128783453+suneelchenna@users.noreply.github.com>
2024-04-18 09:52:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
-module "vpc_services" {
-  source = "./modules/vpc"
-  
-  vpc_cidr_block       = var.vpc_cidr_block
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+}
+
+variable "subnet_cidr_blocks" {
+  description = "List of CIDR blocks for the subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+module "vpc" {
+  source      = "./modules/vpc"
+  vpc_cidr    = var.vpc_cidr_block
+  subnets_cidr = var.subnet_cidr_blocks
+  azs         = var.availability_zones
 }
