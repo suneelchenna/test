916dfa40dfecd515ba9a65e4015ccad98045576c
venkatasuneel <128783453+suneelchenna@users.noreply.github.com>
2024-04-18 09:53:20 +0530

    Update vpc.tf

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,7 +1,43 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+}
 
-  tags = {
-    Name = "MainVPC"
+variable "subnets_cidr" {
+  description = "List of CIDR blocks for the subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "subnets" {
+  count                   = length(var.subnets_cidr)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnets_cidr[count.index]
+  availability_zone       = var.azs[count.index]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
+}
+
+resource "aws_route_table_association" "subnet_associations" {
+  count          = length(aws_subnet.subnets)
+  subnet_id      = aws_subnet.subnets[count.index].id
+  route_table_id = aws_route_table.rt.id
 }
